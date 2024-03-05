640d27e9478536924129fdea7aa499fa45258352
MasterAirscrachDev <airscrach@gmail.com>
2024-03-05 15:34:44 +0800

    start on level 2 first

---
+++ Assets/Scripts/FollowCursor.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+public class FollowCursor : MonoBehaviour
+{
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        //follow the cursor (and make the corners line up) (ussing physcis)
+        Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //the offset is the top left corner (accounting for rotation and scale)
+        Vector3 offset = new Vector3(transform.localScale.x / 2, -transform.localScale.y / 2, 0);
+        //rotate the offset
+        offset = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z) * offset;
+
+        Vector3 Tpos = cursorPos + offset;
+        //rotate to 0
+        
+
+        //move towards the cursor
+        float distance = Vector3.Distance(Tpos, transform.position);
+        distance = Mathf.Clamp(distance, 0.5f, 5);
+        rb.AddForce((Tpos - transform.position) * distance);
+        
+    }
+}
---
+++ Assets/Scripts/TextureKeeper.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureKeeper : MonoBehaviour
+{
+    [SerializeField] List<MonoTexture> textures;
+
+    public Sprite GetTexture(string name){
+        foreach(MonoTexture texture in textures){
+            if(texture.name == name){
+                return Sprite.Create(texture.texture, new Rect(0, 0, texture.texture.width, texture.texture.height), new Vector2(0.5f, 0.5f));
+            }
+        }
+        return null;
+    }
+    public Sprite AddTexture(Texture2D texture, string name){
+        //convert every pixel that is not transparent to white
+        Texture2D newTexture = new Texture2D(texture.width, texture.height);
+        for(int x = 0; x < texture.width; x++){
+            for(int y = 0; y < texture.height; y++){
+                if(texture.GetPixel(x, y).a > 0.5f){
+                    newTexture.SetPixel(x, y, Color.white);
+                }else{
+                    newTexture.SetPixel(x, y, Color.clear);
+                }
+            }
+        }
+        newTexture.Apply();
+        textures.Add(new MonoTexture(newTexture, name));
+        return Sprite.Create(newTexture, new Rect(0, 0, newTexture.width, newTexture.height), new Vector2(0.5f, 0.5f));
+    }
+
+}
+[System.Serializable]
+class MonoTexture{
+    public Texture2D texture;
+    public string name;
+    public MonoTexture(Texture2D texture, string name){
+        this.texture = texture;
+        this.name = name;
+    }
+}
