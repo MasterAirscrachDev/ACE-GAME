fc5f2d4bc558ef97759ec2b0c991e069d40ff9a0
MasterAirscrachDev <airscrach@gmail.com>
2024-03-05 17:17:19 +0800

    First box

--- Assets/Scripts/Chromaterator.cs
+++ Assets/Scripts/Chromaterator.cs
@@ -6,8 +6,9 @@ public class Chromaterator : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] GameObject colorPrefab;
-    [SerializeField] float distance = 0.5f;
+    [SerializeField] float distance = 0.5f, scale = 1.0f, spinSpeed = 0.0f;
     [SerializeField] List<ColorObj> colors;
+    float spin = 45;
     void Start()
     {
         
@@ -16,10 +17,11 @@ public class Chromaterator : MonoBehaviour
     {
         for(int i = 0; i < colors.Count; i++){
             //position the color at an offset based on a rotation derived from i
-            float angle = 360 / colors.Count * i + 45;
+            float angle = 360 / colors.Count * i + spin;
             colors[i].transform.position = transform.position + new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad), 0) * distance;
             colors[i].transform.Translate(new Vector3(0, 0, 1 + (i * 0.02f)));
             colors[i].transform.rotation = transform.rotation;
+            spin += spinSpeed * Time.deltaTime; if(spin > 360){spin -= 360;}
         }
     }
     public bool IsColor(Color color){
@@ -41,7 +43,7 @@ public class Chromaterator : MonoBehaviour
                 s = textureKeeper.AddTexture(sr.sprite.texture, sr.sprite.name);
             }
             newColor.GetComponent<SpriteRenderer>().sprite = s;
-            newColor.transform.localScale = transform.localScale;
+            newColor.transform.localScale = transform.localScale * scale;
             newColor.transform.position = transform.position; //position the color at the center of the chromaterator then move back 1 unit
             newColor.transform.Translate(new Vector3(0, 0, 1 + (colors.Count * 0.02f)));
             colors.Add(new ColorObj{color = color, transform = newColor.transform});
